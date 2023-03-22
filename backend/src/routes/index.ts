235f1514fdfc56f5df362415a82e4545634e151f
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2023-03-22 14:22:37 +0300

    add crash-test

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,26 +1,30 @@
+import { Router, Request, Response, NextFunction } from "express";
+import userRouter from "./users";
+import cardRouter from "./cards";
+import auth from "../middlewares/auth";
+import NotFoundError from "../errors/not-found-error";
+import { createUser, login } from "../controllers/users";
 import {
-  Router, Request, Response, NextFunction,
-} from 'express';
-import userRouter from './users';
-import cardRouter from './cards';
-import auth from '../middlewares/auth';
-import NotFoundError from '../errors/not-found-error';
-import {
-  createUser, login,
-} from '../controllers/users';
-import { validateUserBody, validateAuthentication } from '../middlewares/validatons';
+  validateUserBody,
+  validateAuthentication,
+} from "../middlewares/validatons";
 
 const router = Router();
-router.post('/signup', validateUserBody, createUser);
-router.post('/signin', validateAuthentication, login);
+router.post("/signup", validateUserBody, createUser);
+router.post("/signin", validateAuthentication, login);
+router.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Сервер сейчас упадёт");
+  }, 0);
+});
 
 // все роуты, кроме /signin и /signup, защищены авторизацией;
 router.use(auth);
-router.use('/users', userRouter);
-router.use('/cards', cardRouter);
+router.use("/users", userRouter);
+router.use("/cards", cardRouter);
 
 router.use((req: Request, res: Response, next: NextFunction) => {
-  next(new NotFoundError('Маршрут не найден'));
+  next(new NotFoundError("Маршрут не найден"));
 });
 
 export default router;
