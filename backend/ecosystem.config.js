7394229b709c14f7679a4dc02bfde0b03577c888
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2023-03-22 12:06:36 +0300

    Update ecosystem.config.js

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -24,9 +24,9 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      "local-pre-deploy": `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      "local-pre-deploy": `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
       "post-deploy":
-        "cd /home/walter2032/mesto-backend/source/backend && npm i && npm run build",
+        "cd /home/walter2032/web-plus-pm2-deploy/source/backend && npm i && npm run build",
     },
   },
 };
