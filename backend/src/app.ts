3d9b791342acc7d4883795e0170071c86398d552
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2023-03-22 13:11:59 +0300

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,20 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-import express from 'express';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
-import { errors } from 'celebrate';
-// import cors from 'cors';
-import errorHandler from './middlewares/error-handler';
-import { DB_ADDRESS } from './config';
-import routes from './routes';
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import { errors } from "celebrate";
+import cors from "cors";
+import errorHandler from "./middlewares/error-handler";
+import { DB_ADDRESS } from "./config";
+import routes from "./routes";
 
 const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect(DB_ADDRESS);
 
-// Только для локальных тестов. Не используйте это в продакшене
-// app.use(cors())
+app.use(cors({ origin: "vladislav-front.students.nomoredomains.work" }));
+app.use;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -23,4 +23,4 @@ app.use(errors());
 app.use(errorHandler);
 
 // eslint-disable-next-line no-console
-app.listen(PORT, () => console.log('ok'));
+app.listen(PORT, () => console.log("ok"));
